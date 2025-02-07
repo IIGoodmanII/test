a2054e8ca45cf5c6438c5559732ca725c70cad8d
Чеботарь Комаров <29.nik@mail.ru>
2025-02-07 10:52:57 +0300

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,1 +1,26 @@
-print("Helloy Word")
+print("1 - Задача 1 ")
+print("2 - Задача 2 ")
+print("3 - Задача 3 ")
+print("4 - Задача 4 ")
+k=int(input("Введите номер задачи: "))
+if k==1:
+	a=int(input("Введите А: "))
+	b=int(input("Введите В: "))
+	print("A + B = ",a+b)
+elif k==2:
+	a=int(input("Введите А: "))
+	b=int(input("Введите В: "))
+	print("A - B = ",a-b)
+if k==3:
+	a=int(input("Введите А: "))
+	b=int(input("Введите В: "))
+	print("A * B = ",a*b)
+if k==4:
+	a=int(input("Введите А: "))
+	b=int(input("Введите В: "))
+	print("A / B = ",a/b)
+else:
+	print("Неверный номер задачи")
+
+
+
